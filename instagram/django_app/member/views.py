06f409da1c86bab1ba091dbf481a6a98cbd333c6
kthanterran@gmail.com <kthanterran@gmail.com>
2016-11-15 16:56:26 +0900

    upload signup function v.Beta

--- instagram/django_app/member/views.py
+++ instagram/django_app/member/views.py
@@ -18,6 +18,39 @@ class MemberLoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput())
 
 
+class MemberSignupForm(forms.ModelForm):
+    class Meta:
+        model = MyUser
+        fields = ("username", "password", "first_name", "last_name","email",)
+        widgets = {"password": forms.PasswordInput()}
+
+
+class MemberSignup(FormView):
+    form_class = MemberSignupForm
+    template_name = "member/signup.html"
+    success_url = reverse_lazy("photo:photo_list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = self.get_form()
+        return context
+
+    def form_valid(self, form):
+        myuser = MyUser.objects.create_user(username=form.cleaned_data["username"],
+                                          password=form.cleaned_data["password"],
+                                          email=form.cleaned_data["email"],
+                                          first_name=form.cleaned_data["first_name"],
+                                          last_name=form.cleaned_data["last_name"])
+        user = authenticate(username=form.cleaned_data["username"],
+                            password=form.cleaned_data["password"])
+
+        # next_url = self.request.GET.get("next")
+
+        if user is not None:
+            login(self.request, user)
+            return super().form_valid(form)
+
+
 class MemberLogin(FormView):
     form_class = MemberLoginForm
     template_name = "member/login.html"
