0f565915d24b4a702a2c33479afbdaaed1e52359
Albert Han <alberthan92@gmail.com>
2016-11-15 16:58:29 +0900

    made url connections for LogoutView

--- instagram/django_app/member/urls.py
+++ instagram/django_app/member/urls.py
@@ -4,6 +4,7 @@ from member import views
 
 urlpatterns = [
     url(r'login/$', views.MemberLogin.as_view(), name="login"),
-    url(r'logout/$', views.logout, name="logout"),
+    # url(r'logout/$', views.logout, name="logout"),
+    url(r'logout/$', views.LogoutView.as_view(), name="logout"),
     # url(r'login/$', views.login_fbv, name="login"),
 ]
